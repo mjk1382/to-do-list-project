925a84a07c88f5b0b92fef4ebb26cef35c6aafd4
javad <mohammadjavadkashkooli@gmail.com>
2025-03-27 15:52:49 +0330

    project hads kalme ba try khodam

--- src/App.js
+++ src/App.js
@@ -3,41 +3,14 @@ import TopForm from './TopForm';
 import TaskItems from './TaskItems';
 import { Taskcontext } from './taskContext';
 const App = ()=>{
-    const [Task ,setTask]=useState([
 
-        {
-        id:1,
-        title:"کار شماره2",
-        done:false
-    },
-    {
-        id:4,
-        title:"کار شماره3",
-        done:true
 
-    },
-    {
-        id:3,
-        title:"کار شماره4",
-        done:false
-
-    },
-
-])
     return (
      
         <div className="container w-100 h-100 p-3">
-            <div className="row h-100 justify-content-center align-align-items-start">
-                <div className="col-12 col-md-8 col-lg-6 bg-light shadow rounded-3 p-3 h_fit">
-                <Taskcontext.Provider value={{
-                    Task,
-                    setTask
-                }}>
+            
                     <TopForm />
-                    <TaskItems />
-                </Taskcontext.Provider>
-                </div>
-            </div>
+          
         </div>
       
     ) 
--- src/TopForm.jsx
+++ src/TopForm.jsx
@@ -1,29 +1,48 @@
-import React, { useContext, useState } from 'react';
-import { Taskcontext } from './taskContext';
+import React, { useState, useRef, useEffect } from "react";
+let counter=0
 
-const TopForm = ()=>{
-    const [form , setForm]= useState("")
-    const{Task,setTask}=useContext(Taskcontext)
-    
-     const handelsetForm=(event)=>{
-        
-        setForm(event.target.value)
-     }
-     const AddItem=(event)=>{
-        event.preventDefault()
-        setTask([...Task,{id : Math.random(), title:form , done:false}])
-     }
-    return(
-        <>
-            <h4 className="text-center text-info text_shdow">پروژه</h4>
-            <form onSubmit={AddItem}>
-                <div className="form-group d-flex">
-                    <input type="text" className="form-control" value={form} onChange={handelsetForm} />
-                    <button   type="submit" className="btn btn-success me-1">ثبت</button>
-                </div>
-            </form>
-        </>
-    )
-}
+const Hads = () => {
 
-export default TopForm;
+  const [name, setName] = useState("");
+  const myInput = useRef();
+
+  const handleSetName = () => {
+    setName(myInput.current.value);
+  };
+  useEffect(()=>{
+    myInput.current.focus()
+  },[])
+
+  return (
+    <div>
+      <h4 className="text-3xl font-bold underline">حدس کلمه</h4>
+
+      <input
+        type="text"
+        ref={myInput} 
+        className="bg-red-50 border border-red-500 text-red-900 placeholder-red-700 text-sm rounded-lg focus:ring-red-500 dark:bg-gray-700 focus:border-red-500 block w-full p-2.5 dark:text-red-500 dark:placeholder-red-500 dark:border-red-500"
+        placeholder="Error input"
+      />
+
+      <p className="mt-2 text-sm text-red-600 dark:text-red-500">
+        <span className="font-medium">Oh, snap!</span> Some error message.
+      </p>
+
+      <button onClick={handleSetName} className="bg-pink-500 p-2 rounded">
+        ثبت
+      </button>
+
+      <button
+        onClick={() => (myInput.current.value = "")}
+        className="bg-gray-400 p-2 rounded ml-2"
+      >
+        مخفی
+      </button>
+
+      <br />
+      <span>{counter++}</span>
+    </div>
+  );
+};
+
+export default Hads;
